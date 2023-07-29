088f599783b3be92989df251dad8ab259c4e5ee5
john.alamina <john.alamina@gmail.com>
2023-07-29 16:33:02 +0100

    add await pc

--- src/app/api/tanksim/route.js
+++ src/app/api/tanksim/route.js
@@ -4,7 +4,7 @@ import TankModel from './tankmodel';
 const sim = new TankModel();
 
 export async function GET (request ){
-  sim.render();
-  let pc = sim.mainTank.pc;
+  await sim.render();
+  let pc = await sim.mainTank.pc;
   return new Response(JSON.stringify(pc));
 }
