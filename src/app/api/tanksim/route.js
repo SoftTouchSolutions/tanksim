291e847438a4578b512ea2c2380889841bed1bec
john.alamina <john.alamina@gmail.com>
2023-07-29 17:07:17 +0100

    add sim singleton

--- src/app/api/tanksim/route.js
+++ src/app/api/tanksim/route.js
@@ -1,9 +1,16 @@
 
 import TankModel from './tankmodel';
-
-const sim = new TankModel();
-
+var sim;
+function getInstance(){
+    if (instance == null) {
+        instance = new TankModel();
+        // Hide the constructor so the returned object can't be new'd...
+        instance.constructor = null;
+    }
+    return instance;
+}
 export async function GET (request ){
+  sim = getInstance();
   await sim.render();
   let pc = await sim.mainTank.pc;
   return new Response(JSON.stringify(pc));
